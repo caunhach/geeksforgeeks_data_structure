cec32fa1e2db87dff726e2691811d0848eb196e0
caunhach <caunhach@LAPTOP-5U4J17VN.localdomain>
2023-01-03 08:49:40 +0700

    Last Updated 01/03/2023

---
+++ Array/Easy/ex00_rearrange_arr/javascript/rearrange_arr.js
@@ -0,0 +1,21 @@
+function rearrange_arr(arr, n){
+    var i, tmp
+    for (i = 0; i < n; i++){
+        while (arr[i] != -1 && arr[i] != i)
+        {
+            tmp = arr[i]
+            arr[i] = arr[tmp]
+            arr[tmp] = tmp
+        }
+    }
+    return arr
+}
+
+/*
+let arr = [-1, -1, 6, 1, 9, 3, 2, -1, 4, -1]
+let n = arr.length
+console.log(rearrange_arr(arr, n))
+arr = [19, 7, 0, 3, 18, 15, 12, 6, 1, 8, 11, 10, 9, 5, 13, 16, 2, 14, 17, 4]
+n = arr.length
+console.log(rearrange_arr(arr, n))
+*/
